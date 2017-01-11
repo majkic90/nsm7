252456843324e5f6c0c9b6f608a63bbc30319acb
Aleksandar Majkic <aleksandar@comdata.rs>
2017-01-11 16:23:26 +0100

    Initing

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var refreshTime = 12000;
 var refOneItem = 4;
 var knifes = [];
 var price = [];
-var startTime = 6;
+var startTime = 7;
 
 io.on('connection', function (socket) {
     socket.send("connect");
